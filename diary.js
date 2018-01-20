1f647b5daca29fc18fc83ffb8abc794a99fbba59
called-d <called.0xd@gmail.com>
2018-01-20 17:10:00 +0900

    modify: diary.js; status content

--- diary.js
+++ diary.js
@@ -3,9 +3,16 @@ class Diary {
     this.elem = diary_element;
   }
   static getElementFromStatus(status) {
-    status_article = document.createElement("article");
+    var status_article = document.createElement("article");
     status_article.classList.add('status');
     status_article.classList.add('status_content');
+    var content = document.createElement("div");
+    content.classList.add('status__content');
+    content.insertAdjacentHTML(
+      'afterbegin',
+      status['content']
+    );
+    status_article.appendChild(content);
     return status_article;
   }
   appendStatus(status) {
