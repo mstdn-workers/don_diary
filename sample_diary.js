b2ca61744296b081435c461d868f9f3a7a84e36f
called-d <called.0xd@gmail.com>
2018-01-20 21:05:15 +0900

    add sample button

--- sample_diary.js
+++ sample_diary.js
@@ -2,4 +2,11 @@ var diary;
 
 document.addEventListener("DOMContentLoaded", function(event) {
   diary = new Diary(document.getElementById("input_area"));
+  var button = document.getElementById("add_status");
+  button.addEventListener("click", function(event) {
+    if (sample_statuses.length > 0) {
+      diary.appendStatus(sample_statuses.pop());
+      diary.maintainInputArea();
+    };
+  });
 });
